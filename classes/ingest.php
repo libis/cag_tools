020f183c17fd329087f1bfedf27e32c398932911
Sam Alloing <sam.alloing@gmail.com>
2014-03-12 14:47:44 +0100

    ingest updated

--- classes/ingest.php
+++ classes/ingest.php
@@ -1,3 +1,3 @@
 <?php
-exec("php crkc_afbeeldingen_ingest.php > /www/libis/web/lias_html/collectiveaccess/crkc_media/crkc_afbeeldingen_ingest.log 2> /www/libis/web/lias_html/collectiveaccess/crkc_media/error.txt");
+exec("php cag_afbeeldingen_ingest.php > /www/libis/web/lias_html/collectiveaccess/cag_media/cag_afbeeldingen_ingest.log 2> /www/libis/web/lias_html/collectiveaccess/cag_media/error.txt");
 ?>
